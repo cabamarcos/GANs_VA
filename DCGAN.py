d8b133299c1e0a213e37accb22985acdeeb7218f
cabamarcos <marcos.caballero.0203@gmail.com>
2024-04-28 19:35:15 +0200

    refactor

--- DCGAN.py
+++ DCGAN.py
@@ -19,7 +19,14 @@ class Discriminator(nn.Module):
     
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False,),
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(0.2),
         )
@@ -30,29 +37,37 @@ class Discriminator(nn.Module):
 class Generator(nn.Module):
     def __init__(self, channels_noise, channels_img, features_g):
         super(Generator, self).__init__()
-        self.gen = nn.Sequential(
+        self.net = nn.Sequential(
             # Input: N x channels_noise x 1 x 1
             self._block(channels_noise, features_g * 16, 4, 1, 0),  # img: 4x4
             self._block(features_g * 16, features_g * 8, 4, 2, 1),  # img: 8x8
             self._block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
             self._block(features_g * 4, features_g * 2, 4, 2, 1),  # img: 32x32
             nn.ConvTranspose2d(
-                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1,
+                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
             ),
             # Output: N x channels_img x 64 x 64
-            nn.Tanh(), # Normalize inputs to [-1, 1] so make outputs [-1, 1]
+            nn.Tanh(),
         )
-    
+
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False,),
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
     
     def forward(self, x):
-        return self.gen(x)
-    
+        return self.net(x)
+
+
 def initialize_weights(model):
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
