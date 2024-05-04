fa6613a9807d124d3efc38e9c01f887ee304ab83
cabamarcos <marcos.caballero.0203@gmail.com>
2024-05-05 00:30:48 +0200

    Also resize in both wgan. Try tomorrow

--- WGAN_train.py
+++ WGAN_train.py
@@ -15,8 +15,8 @@ LEARNING_RATE = 5e-5
 BATCH_SIZE = 64
 IMAGE_SIZE = 64
 CHANNELS_IMG = 3
-Z_DIM = 128
-NUM_EPOCHS = 25
+Z_DIM = 100
+NUM_EPOCHS = 5
 FEATURES_CRITIC = 64
 FEATURES_GEN = 64
 CRITIC_ITERATIONS = 5
@@ -24,7 +24,7 @@ WEIGHT_CLIP = 0.01
 
 transforms = transforms.Compose(
     [
-        transforms.Resize(IMAGE_SIZE),
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
         transforms.ToTensor(),
         transforms.Normalize(
             [0.5 for _ in range(CHANNELS_IMG)], [0.5 for _ in range(CHANNELS_IMG)]
@@ -58,11 +58,10 @@ for epoch in range(NUM_EPOCHS):
     # Target labels not needed! <3 unsupervised
     for batch_idx, (data, _) in enumerate(tqdm(loader)):
         data = data.to(device)
-        cur_batch_size = data.shape[0]
 
         # Train Critic: max E[critic(real)] - E[critic(fake)]
         for _ in range(CRITIC_ITERATIONS):
-            noise = torch.randn(cur_batch_size, Z_DIM, 1, 1).to(device)
+            noise = torch.randn(BATCH_SIZE, Z_DIM, 1, 1).to(device)
             fake = gen(noise)
             critic_real = critic(data).reshape(-1)
             critic_fake = critic(fake).reshape(-1)
@@ -84,15 +83,13 @@ for epoch in range(NUM_EPOCHS):
 
         # Print losses occasionally and print to tensorboard
         if batch_idx % 100 == 0 and batch_idx > 0:
-            gen.eval()
-            critic.eval()
             print(
                 f"Epoch [{epoch}/{NUM_EPOCHS}] Batch {batch_idx}/{len(loader)} \
                   Loss D: {loss_critic:.4f}, loss G: {loss_gen:.4f}"
             )
 
             with torch.no_grad():
-                fake = gen(noise)
+                fake = gen(fixed_noise)
                 # take out (up to) 32 examples
                 img_grid_real = torchvision.utils.make_grid(
                     data[:32], normalize=True
@@ -105,5 +102,3 @@ for epoch in range(NUM_EPOCHS):
                 writer_fake.add_image("Fake", img_grid_fake, global_step=step)
 
             step += 1
-            gen.train()
-            critic.train()
